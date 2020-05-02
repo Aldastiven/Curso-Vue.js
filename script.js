9bd8c171c6c28c84639d060b67731fa1b0a1039f
Aldair Stiven Sierra Ardila <assierra@eliteflower.com>
2020-05-02 16:37:31 -0500

    1. Realizando crud en vue.js con localStorage (implementa json)

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
 const app = new Vue({
     el: '#app',
     data: { 
-        fondo: 'bg-warning',
-        color: false
+        tareas : [],
+        nuevaTarea:''
     },
     methods: {
+        
+        agregarTarea(){                
+            this.tareas.push({
+                nombre : this.nuevaTarea,
+                estado : false
+            });        
+            this.nuevaTarea = '';
+            localStorage.setItem('gym-vue',JSON.stringify(this.tareas));
+        },
 
+        editarTarea(index){
+            this.tareas[index].estado = true;
+            localStorage.setItem('gym-vue',JSON.stringify(this.tareas));
+        },
+
+        eliminarTarea(index){
+            this.tareas.splice(index,1);
+            localStorage.setItem('gym-vue',JSON.stringify(this.tareas));
+        }
+        
     },
     computed: {
         
+    },
+    created: function(){
+        let datosDB = JSON.parse(localStorage.getItem('gym-vue'));
+        this.tareas = (datosDB === null ? [] : datosDB);
     }
-})
+});
