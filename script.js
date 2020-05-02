e08c0602a7a5e139b4e2bc5e51cc1368e5a7ecd8
Aldair Stiven Sierra Ardila <assierra@eliteflower.com>
2020-05-02 14:48:58 -0500

    1. Clase de vue implementando logica con boostra

--- script.js
+++ script.js
@@ -1,43 +1,13 @@
 const app = new Vue({
     el: '#app',
-    data: {
-
-        titulo : 'Agregar Nuevas frutas',
-        frutas : [
-            {nombre : 'pera', cantidad : 0},
-            {nombre : 'manzana', cantidad : 10},
-            {nombre : 'platano', cantidad : 11}
-        ],
-        nuevaFruta : '',
-        totalfruta : 0
-    
+    data: { 
+        fondo: 'bg-warning',
+        color: false
     },
     methods: {
 
-        agregarFruta(){
-            campoFruta = this.nuevaFruta;
-            frutaAgregada = this.frutas;
-            if(campoFruta != ''){
-                frutaAgregada.push({
-                    nombre: campoFruta,
-                    cantidad: 0
-                });
-                this.nuevaFruta = '';            
-            }
-        }
-
     },
     computed: {
-
-        sumarCantidad(){
-            
-            this.totalfruta = 0;
-            for(fruta of this.frutas){
-                this.totalfruta = this.totalfruta + fruta.cantidad;
-            }
-            return this.totalfruta;
-        }
-
+        
     }
-
 })
