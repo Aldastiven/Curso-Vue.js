7a685c78d4265303274329a128c2436c89b272fe
Aldair Stiven Sierra Ardila <assierra@eliteflower.com>
2020-05-02 20:26:01 -0500

    Creacion de componentes con Vue.js

---
+++ componets/texto.js
@@ -0,0 +1,14 @@
+
+Vue.component('texto',
+{
+    template:
+    /*html*/
+    `<div>
+        <h3>{{dato}}</h3>
+    </div>`,
+    data(){
+         return {
+             dato : 'este es un texto'
+         }
+     }
+});
